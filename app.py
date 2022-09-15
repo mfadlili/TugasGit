797f87234b729098470a5f2fa4545ba1dcb7eec8
John Doe <fadlil.ismail@gmail.com>
2022-09-15 22:10:12 +0700

    open data.txt in app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 from fungsi import kuadrat, akar
+
+test_data = []
+with open("data.txt", "r") as file:
+    test = file.readlines()
+    for row in test:
+        test_data.append(int(row))
+
+    
